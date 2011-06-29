4300b7dfeed6c63b9c2ef7177f94fccf07e27aa5
bestwebsoft <bestwebsoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-29 13:41:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/contact-form-plugin/tags/2011.05.26@402426 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- contact_form.php
+++ contact_form.php
@@ -6,7 +6,7 @@
 /*
 Plugin Name: Contact Form Plugin
 Plugin URI:  http://bestwebsoft.com/plugin/
-Description: Plugin for portfolio.
+Description: Plugin for Contact Form.
 Author: BestWebSoft
 Version: 1.0
 Author URI: http://bestwebsoft.com/
